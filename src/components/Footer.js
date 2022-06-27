47280f290ceac880c9ce256d511460b0784eda45
kabirrajsingh99 <kabirrajsingh10@gmail.com>
2022-06-28 00:24:18 +0530

    ups

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
                             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                                 <svg className="bi" width="30" height="24"></svg>
                             </a>
-                            <span className="mb-3 mb-md-0">© 2022 Company, Inc</span>
+                            <span className="mb-3 mb-md-0">© 2022 Krypto, Made by Kabir</span>
                         </div>
 
                         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
